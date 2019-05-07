191f89677e593e8dd4b920ddc34b26d25cd3b8be
Raymundo <A00570654@itesm.mx>
2019-05-06 19:49:29 -0500

    HUD
    HUD implementado

--- src/Game.java
+++ src/Game.java
@@ -1,235 +1,164 @@
 import java.awt.Color;
 import java.awt.Dimension;
-
-import image.Assets;
-import java.awt.Graphics;
+import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferStrategy;
+import java.util.*;
+
+import javax.swing.Timer;
 
 import image.Assets;
 
-// Clase principal para que el juego corra
-/*
- * Implementa la clase Runnable, la cual corre al m�todo run() 
- * cuando se inicia el thread princilal 
- */
-
-public class Game implements Runnable{
-	// Variables que determinan el ancho, el alto y el t�tulo del juego
+public class Game implements Runnable {
 	public static int width, height;
 	public String title;
+	public int tiempo = 120;
 	
-	// �ste es el thread principal sobre el cu�l va a correr el juego
 	public Thread thread;
-	
-	// Se crean variables de las clases que creamos
 	private Window window;
 	private Handler handler;
 	private KeyInput keyInput;
-	
-	// �stas 2 variables nos van a ayudar a hacer el renderizado en el juego
 	private BufferStrategy bs;
 	private Graphics2D g;
 	private LevelCreator level;
 	private Player player;
 	private Rocket rocket;
-	
-	// Con la variable booleana
+	private HUD HUD;
 	private boolean running = false;
 	
-	// Se crea el constructor de la clase, recibe el tama�o de la ventana y su t�tulo
-	public Game(String title, int width, int height)
-	{
+	Menu menu = new Menu(720,480);
+	Color color = new Color(150,150,150);
+	Font font = new Font("Times New Roman",Font.PLAIN,36);
+	//Timer timer = new Timer(1000,AE->segundoMenos());
+	
+	//Constructor
+	public Game(String title, int width, int height) {
 		this.title = title;
 		Game.width = width;
 		Game.height = height;
 	}
 	
-	// M�todo que inicializa los recursos que ser�n utilizados a lo largo del juego
-	public void init()
-	{
-		// Inicializa todas las im�genes (se encuentran en Assets)
-		/*
-		 * Hacer uso de Assets nos da la ventaja de no tener que estar cargando
-		 * las im�genes cada que sean ocupadas, sino que solo se cargan una vez
-		 * y son referenciadas cuando se requieran usar.
-		 */
+	//Inicializador
+	public void init() {
 		Assets.init();
-		
-		// Se crea la ventana, d�ndole su width, height y el t�tulo del juego
+	
 		window = new Window(title, width, height);
-		// Se crea el Handler 
-		/*
-		 * Handler nos ayuda a manejar los updates y renderizados de los objetos
-		 * contenidos en el juego
-		 */
 		handler = new Handler();
-		//se creal el nivel
-		level = new LevelCreator (handler);
-		// Se crea al jugador
-		player = new Player(80, 200, 32, 32, Assets.tankU, handler);
-		rocket = new Rocket(80, 250, 32, 32, Assets.rocketPU, handler);
-		// Se crea el KeyInput
-		/*
-		 * En nuestro caso, qui�n se har� cargo de escuchar los inputs recibidos
-		 * por el usuario, es el jugador.
-		 */
+		level = new LevelCreator(handler);
+		player = new Player(80,200,32,32,Assets.tankU, handler);
+		rocket = new Rocket(80,250,32,32,Assets.rocketPU, handler);
 		keyInput = new KeyInput(player);
-		// A�ade el KeyInput a la ventana, para que �sta sea la intermediaria
 		window.getFrame().addKeyListener(keyInput);
-		
-		// Se le a�aden los objetos pared y jugador al Handler
 		handler.addObj(player);
 		handler.addObj(rocket);
+		HUD = new HUD(0,452,720,30);
 	}
 	
-	// El m�todo start se encarga de iniciar al juego
-	public synchronized void start()
-	{
-		System.out.println("Hit the special block to win the game\nShoot with space, move with the arrows");
-		// Si el juego se est� corriendo, no hace nada
+	//Inicializador del juego
+	public synchronized void start() {
+		System.out.println("May the game begin");
 		if (running) return;
-		// Hace que running pase al estado de true e inicia el thread principal
 		running = true;
 		thread = new Thread(this);
 		thread.start();
+		/*
+		if(menu.getModo()==2) {
+			timer.start();
+		}
+		*/
 	}
 	
-	// El m�todo stop se encarga de detener al juego
-	public synchronized void stop()
-	{
-		// Si el juego no est� corriendo, no hace nada
+	//Finalizador del juego
+	public synchronized void stop() {
 		if (!running) return;
-		// Hace que running pase al estado de false y termina el thread principal
 		running = false;
-		try
-		{
+		try {
 			thread.join();
-		}
-		catch(InterruptedException e)
-		{
+		} catch(InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	// El m�todo tick() se encarga de realizar el update del juego
-	public void tick()
-	{
+	//Actualizador del juego 
+	public void tick() {
 		if (handler.isWin())
 			running=false;
-			// Se llama al m�todo tick del Handler y del Spawner
 		handler.tick();
 	}
 	
-	// El m�todo render() se encarga de hacer el renderizado del juego
-	public void render()
-	{
+	//Render
+	public void render() {
 		// Se obtiene el BufferStrategy que tiene el Canvas de Window
 		bs = window.getCanvas().getBufferStrategy();
 		
-		// Si Canvas no tiene un BufferStrategy
-		if (bs == null)
-		{
-			// Se le crea uno de 3 Buffers y retorna
+		//Creador del BufferStrategy
+		if (bs == null) {
 			window.getCanvas().createBufferStrategy(3);
 			return;
 		}
-		// Graphics obtiene lo que est� dibujado en el BufferStrategy
 		g = (Graphics2D) bs.getDrawGraphics();
-		// Se limpia el Buffer para dibujar en blanco
 		g.clearRect(0, 0, width, height);
+		//g.drawImage(Assets.background, 0, 0, null);
 		
-		// Obtiene tama�os de la pantalla
+		//Escalado de pantalla
 		Dimension scrSize = Toolkit.getDefaultToolkit().getScreenSize();
 		double scrWidth = scrSize.getWidth();
 		double scrHeight = scrSize.getHeight();
-		
-		
-		// Dibuja el fondo
-		//g.drawImage(Assets.background, 0, 0, null);
 		g.drawImage(Assets.background, 0, 0, (int)scrWidth, (int)scrHeight, null);
-		
-		// Scale the image
 		double wScale = scrWidth/width;
-		double aproxH = (height*15)/16;
+		double aproxH = (height*17)/17;
 		double hScale = (scrHeight/aproxH);
 		AffineTransform at = new AffineTransform();
 		at.scale(wScale, hScale);
 		g.setTransform(at);
 		
-		
-		// Pinta los objetos contenidos en el handler
+		//Pinta los objetos del Handler
 		handler.render(g);
-		
-		// Deja de hacer uso del "pincel" para no consumir los recursos todo el tiempo
+		HUD.render(g);
 		g.dispose();
-		// Muestra lo que Graphics dibuj� en los Buffers
 		bs.show();
-		
 	}
+	/*
+	public void segundoMenos() {
+		tiempo = tiempo - 1;
+	}
+	*/
 	
-	
-	// �ste es el m�todo primordial para el GameLoop
-	public void run()
-	{
-		// Llama a init() para inicializar lo que es requerido en el juego
+	//Runnable
+	public void run() {
 		init();
-		// Se declara un target de actualizaciones por segundo con fps
-		// Con ticks se hace tracking de las actualizaciones que se hacen por segundo
 		int fps = 60, ticks = 0;
-		// timePerTick nos muestra cu�nto tiempo debe pasar entre cada update
-		// timePerTick es 1000000000 porque se manejar� el tiempo en nano segundos
-		// delta nos ayuda a saber si el tiempo de timePerTick ya ha pasado
 		double timePerTick = 1000000000 / fps, delta = 0;
-		// now nos da el tiempo actual y lastTime el tiempo anterior
-		// timer da seguimiento al tiempo que ha pasado desde el �ltimo ciclo
 		long now, lastTime = System.nanoTime(), timer = 0;
-		
-		// Mientras el juego se encuentre corriendo...
-		while (running)
-		{
-			// Se obtiene el tiempo actual
+
+		while (running) {
 			now = System.nanoTime();
-			// Se le suma a delta para ver si es momento de hacer un update
 			delta += (now-lastTime) / timePerTick;
-			// Se le suma a timer el tiempo que se lleva
 			timer += now - lastTime;
-			// lastTime avanza a ser el actual
 			lastTime = now;
-			
-			// Si el delta es mayor o igual a uno
-			if (delta >= 1)
-			{
-				// Se realiza el update y el pintado
+
+			if (delta >= 1) {
 				tick();
 				render();
-				// Se le suma uno a ticks para dar seguimiento a las actualizaciones que
-				// se llevan en �ste segundo
-				ticks ++;
-				// Se reduce delta en uno para que se pueda volver a realizar el update
-				delta --;
+				ticks++;
+				delta--;
 			}
-			
-			// �sto significa que si ha pasado ya un segundo
-			if (timer >= 1000000000)
-			{
-				// Imprime el n�mero de actualizaciones que se hicieron en ese segundo
-				//System.out.println("Ticks and frames: " + ticks);
-				// Reinicia los contadores
+			if (timer>=1000000000) {
 				ticks = 0;
 				timer = 0;
 			}
 		}
-		// Cuando el juego ya no est� corriendo, se detiene
+		System.out.println("" + tiempo);
 		stop();
-		System.out.println("yeih");
 		System.exit(0);
-	}
-		
-	public int getWidth() { return width; }
-	public int getHeight() { return height; }
+	}	
 	
+	public int getWidth() { 
+		return width; 
+	}
+	public int getHeight() { 
+		return height; 
+	}
 }
--- src/Player.java
+++ src/Player.java
@@ -9,60 +9,58 @@ import java.util.ListIterator;
 
 import image.Assets;
 
-
-
-/*ESTA BRANCH VALE VERGA*/
-
 // Clase que define el comportamiento del jugador
 public class Player extends Chracter{
 
+	private static int vidas = 3;
+	private static int cohetes = 0;
+	private static int puntos = 0;
+	private static double PI = 3.1415;
+	
 	private int dir=1, pack5;
 	private AnimationSprite bat;
-	private static double PI=3.1415;
-	// Constructor que recibe los atributos de un GameObject
-	public Player(int x, int y, int width, int height,  BufferedImage bi, Handler handler) {
-		super(x, y, width, height, handler);
+
+	public Player(int x, int y, int width, int height, BufferedImage bi, Handler handler) {
+		super(x,y,width,height,handler);
 		SpriteBuilder builder = new SpriteBuilder ("/Textures/16dir.png", 32, 32);
-		for (int i=0; i<16 ; i++) {//add all frames
-			builder.addImage(i, 0);
+		for (int i=0; i<16; i++) {
+			builder.addImage(i,0);
 		}
 		bat=new AnimationSprite(x, y, builder.build());
 		bat.setAnimSpd(5);
-		}
-		
-	// M�todo que nos ayuda a actualizar al jugador
-	@Override
-	public void tick() 
-	{	
+	}
+	public int getVidas() {
+		return vidas;
+	}
+	public int getCohetes() {
+		return cohetes;
+	}
+	public int getPuntos() {
+		return puntos;
+	}
+	public void perderVida() {
+		vidas = vidas - 1;
+		//if() {}
+	}
+	public void ganarPunto() {
+		puntos = puntos + 1;
+		//if() {}
+	}
+
+	public void tick() {	
 		bat.update();
 	}
-	
-	// M�todo que se encarga de detectar las colisiones
-	
-	@Override
-	public boolean collision(double dx, double dy) 
-	{
-		// Se genera un iterador para revisar todos los objetos
+	public boolean collision(double dx, double dy) {
 		ListIterator <GameObject> iterator = handler.obj.listIterator();
-		while (iterator.hasNext())
-		{
-			// Se crea un objeto auxiliar
+		while (iterator.hasNext()) {
 			GameObject aux = iterator.next();
-			
-			// Si el auxiliar es una pared
-			if (aux instanceof Block)
-			{
-				// Si hace contacto con la pared en el eje de las x al sumarle la velocidad
-				if (placeMeeting(x+dx, y-dy, aux))
-				{
+			if (aux instanceof Block) {
+				if (placeMeeting(x+dx, y-dy, aux)) {
 					return true;
 				}
 			}
-			if (aux instanceof Rocket)
-			{
-				// Si hace contacto con la pared en el eje de las x al sumarle la velocidad
-				if (placeMeeting(x+dx, y-dy, aux))
-				{
+			if (aux instanceof Rocket) {
+				if (placeMeeting(x+dx, y-dy, aux)) {
 					handler.removeObj(aux);
 					pack5 = 5;
 					return true;
@@ -80,46 +78,40 @@ public class Player extends Chracter{
 			movX=(int)Math.floor((round(Math.cos(direction*(3.1415/180)*22.5)*2,2)));
 		return movX;
 	}
-	
-	public static int moveY (int direction) {
-			int movX;
-			if (direction >= 0 && direction <=7)
-				movX=(int)Math.ceil((round(Math.sin(direction*(3.1415/180)*22.5)*2,2)));
-			else
-				movX=(int)Math.floor((round(Math.sin(direction*(3.1415/180)*22.5)*2,2)));
+	public static int moveY(int direction) {
+		int movX;
+		if (direction >= 0 && direction <=7)
+			movX=(int)Math.ceil((round(Math.sin(direction*(3.1415/180)*22.5)*2,2)));
+		else
+			movX=(int)Math.floor((round(Math.sin(direction*(3.1415/180)*22.5)*2,2)));
 		return movX;
 	}
 	
 	  public static double round(double value, int places) {
-		    if (places < 0) throw new IllegalArgumentException();
+	    if (places < 0) throw new IllegalArgumentException();
 
-		    BigDecimal bd = new BigDecimal(value);
-		    bd = bd.setScale(places, RoundingMode.HALF_UP);
-		    return bd.doubleValue();
+	    BigDecimal bd = new BigDecimal(value);
+	    bd = bd.setScale(places, RoundingMode.HALF_UP);
+	    return bd.doubleValue();
 	  }
 	
-	public void paint(Graphics g) 
-	{		
+	public void paint(Graphics g) {		
 		bat.render(g, x, y, angle);
 	}
 	
-	// M�todo que lee las teclas que han sido presionadas
+	//KeyListener
 	public void keyPressed(int key) {
-		// Si es escape, cierra el juego
 		if (key == KeyEvent.VK_ESCAPE) System.exit(1);
-		// Si es la flecha izquierda, vuelve la velocidad en x -3
 		if (key == KeyEvent.VK_LEFT) { 
 			dir=2;
 			counterClockWise();
 			System.out.println(angle);
-			}
-		// Si es la flecha derecha, vuelve la velocidad en x +3
+		}
 		if (key == KeyEvent.VK_RIGHT) { 
 			dir=4;
 			clockWise();
 			System.out.println(angle);
 		}
-		// Si es la flecha arriba, vuelve la velocidad en y -3
 		if (key == KeyEvent.VK_UP) { 
 			if (!collision(moveX(angle)*2, moveY(angle)*2)) {
 				x+=moveX(angle);
@@ -127,35 +119,28 @@ public class Player extends Chracter{
 				dir=1;
 			}
 		}
-		// Si es la flecha abajo, vuelve la velocidad en y +3
-		if (key == KeyEvent.VK_DOWN) { 
+		if(key == KeyEvent.VK_DOWN) { 
 			if (!collision(moveX((angle+8)%16)*2, moveY((angle+8)%16)*2)) {
 				x+=moveX((angle+8)%16);
 				y-=moveY((angle+8)%16);
 				dir=3;
 			}
 		}
-		
-		if (key == KeyEvent.VK_SPACE) {
+		if(key == KeyEvent.VK_SPACE) {
 			if(pack5>0) {
 				handler.addObj(new RocketBllt(this.getX()+15, this.getY()+16, 8, 8, Assets.Rbullet, moveX(angle), moveY(angle), handler, angle));
 				pack5--;
-			}else
+			} else {
 			handler.addObj(new Bullet(this.getX()+15, this.getY()+16, 8, 8, Assets.bullet, moveX(angle), moveY(angle), handler));
+			}
 		}
-		
 		if (key == 67) {
 			//handler.addObj(new Bomb(this.getX()+15, this.getY()+16, 8, 8,  moveX(angle), moveY(angle), angle, handler));
 		}
-		
 	}
 
 	public void keyReleased(int key) {
-		
 	}
-
-	public void keyTyped(int key) {
-		
+	public void keyTyped(int key) {	
 	}
-
 }
